6a8345be86d811c8a0c7e9019aa71c819a034d60
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-01-29 12:45:02 -0700

    Add radio buttons for all the extrusion types and 3 hole options

--- src/main/java/com/t_slot_cnc/ui/LeftPanel.java
+++ src/main/java/com/t_slot_cnc/ui/LeftPanel.java
@@ -9,6 +9,9 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.t_slot_cnc.controller.MainController;
 import com.t_slot_cnc.model.Extrusion;
 
@@ -20,36 +23,57 @@ public class LeftPanel extends JPanel {
 	/** Serialize */
 	private static final long serialVersionUID = 8978598811708208648L;
 
+	private static final Logger logger = LoggerFactory.getLogger(LeftPanel.class); // Get the SLF4J logger instance
+
 	public LeftPanel(MainController controller, RightPanel rightPanel) {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-		JRadioButton option1 = new JRadioButton("Option 1");
-		JRadioButton option2 = new JRadioButton("Option 2");
-
 		ButtonGroup group = new ButtonGroup();
-		group.add(option1);
-		group.add(option2);
+		List<Extrusion> exts = controller.getExtrusionSeries();
+		//Create radio buttons for each extrusion
+		JRadioButton[] radioExt = new JRadioButton[exts.size()];
+		int i = 0;
+		for (Extrusion ext : exts) {
+			radioExt[i] = new JRadioButton(ext.getId());
+			group.add(radioExt[i]);
+			radioExt[i].addActionListener(e -> controller.selectOption(ext.getId()));
+			add(radioExt[i]);
+			
+			logger.warn("id={} d={}" , ext.getId(), ext.getAccessHole().getDiameter());
+			if (i==0) {
+				//select the first one - we could also save the last choice made and select it
+				radioExt[i].setSelected(true);
+				controller.selectOption(ext.getId());
+			}
 
+			i++;
+		}
+		
+		ButtonGroup holeGroup = new ButtonGroup();
+		JRadioButton accessHoleRadio = new JRadioButton("Access hole");
+		JRadioButton counterboreRadio = new JRadioButton("Counterbore");
+		JRadioButton comboAHCounterRadio = new JRadioButton("Combo Access/Counter");
+		
+		accessHoleRadio.setSelected(true);
+		
+		holeGroup.add(accessHoleRadio);
+		holeGroup.add(counterboreRadio);
+		holeGroup.add(comboAHCounterRadio);
+		
+		//
+		add(Box.createVerticalStrut(10));
+		add(accessHoleRadio);
+		add(counterboreRadio);
+		add(comboAHCounterRadio);
+		
 		JButton okButton = new JButton("OK");
-
-		option1.addActionListener(e -> controller.selectOption("OPTION_1"));
-		option2.addActionListener(e -> controller.selectOption("OPTION_2"));
-
 		okButton.addActionListener(e -> {
 			String text = controller.onOkPressed();
 			rightPanel.setText(text);
 		});
 
-		add(option1);
-		add(option2);
 		add(Box.createVerticalStrut(10));
 		add(okButton);
-
-		//Just for kicks show all the extrusions IDs
-		List<Extrusion> exts = controller.getExtrusionSeries();
-		for (Extrusion ext : exts) {
-			System.out.println(ext.getId() + " d=" + ext.getAccessHole().getDiameter());
-		}
 	}
 
 }
