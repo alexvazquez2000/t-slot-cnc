5b5eea474f824de9bc64adcbeddf006227a86df1
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-02-06 13:00:36 -0700

    split series and hole type

--- src/main/java/com/t_slot_cnc/model/SelectionModel.java
+++ src/main/java/com/t_slot_cnc/model/SelectionModel.java
@@ -4,14 +4,29 @@ package com.t_slot_cnc.model;
  * @author Alex Vazquez <vazqueza2000@gmail.com>
  */
 public class SelectionModel {
-	private String selectedOption;
+	private String selectedSeries;
+	private String selectedHoleType;
 
-	public String getSelectedOption() {
-		return selectedOption;
+	public String getSelectedSeries() {
+		return selectedSeries;
 	}
 
-	public void setSelectedOption(String selectedOption) {
-		this.selectedOption = selectedOption;
+	public void setSelectedSeries(String selectedSeries) {
+		this.selectedSeries = selectedSeries;
+	}
+
+	/**
+	 * @return the selectedHoleType
+	 */
+	public String getSelectedHoleType() {
+		return selectedHoleType;
+	}
+
+	/**
+	 * @param selectedHoleType the selectedHoleType to set
+	 */
+	public void setSelectedHoleType(String selectedHoleType) {
+		this.selectedHoleType = selectedHoleType;
 	}
 
 }
