0369c83af69060415ea87e36ff5fdaeafecb8066
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-02-09 20:14:10 -0700

    Increase depth of cut

--- src/main/java/com/t_slot_cnc/service/MachineService.java
+++ src/main/java/com/t_slot_cnc/service/MachineService.java
@@ -10,8 +10,12 @@ public class MachineService {
 	//30–50 IPM (inches per minute) feed rate, with shallow depths of cut ( ~0.01-0.03").
 	//inches per minute
 	private int feedRate = 40;
-	private int spindleSpeed = 7; //18,000 for 1/8 inch
-	private double cutDepthPerPass = 0.02;
+	//recommended is betwee 10k and 24K.  Speed 7 is 18,000 RPMs
+	//Speed is actually being ignored
+	private int spindleSpeed = 7; 
+	// Recommended Depth of Cut (DOC): 0.01" - 0.03" (10-30% of diameter)
+	private double cutDepthPerPass = 0.05;
+	
 	private double accuracy = 0.02;
 	
 	//z-gap above material - inches
