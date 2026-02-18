8ad97b8b1042c2832e845dabfbdf1b857c428037
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-02-18 15:07:22 -0700

    move files into different subdirectries by type of hole

--- src/main/java/com/t_slot_cnc/service/FileNameService.java
+++ src/main/java/com/t_slot_cnc/service/FileNameService.java
@@ -4,15 +4,34 @@ import com.t_slot_cnc.model.AccessHole;
 import com.t_slot_cnc.model.Counterbore;
 import com.t_slot_cnc.model.Extrusion;
 
+/**
+ * @author Alex Vazquez <vazqueza2000@gmail.com>
+ */
 public class FileNameService {
 
 	private static String fileExtension = ".nc";
+	
+	private FileNameService() {
+		//Don't instantiate - only static members
+	}
 
 	public static String nameCounterbore(Extrusion ext, Counterbore counterbore, int numColumns) {
 		//fileName +=  "X_cb_A_" + counterbore.getPartNumber() + fileExtension
 		//fileName += "X_cb_A_B_C_D_" + counterbore.getPartNumber() + fileExtension
 		
-		return "output/" + ext.getId() + "/" + ext.getId().substring(0,2) + "X_cb" + columnPattern(numColumns) + fileExtension;
+		return "output/" + ext.getId() + "/counterbore/" + ext.getId().substring(0,2) + "X_cb" + columnPattern(numColumns) + fileExtension;
+	}
+
+	public static String nameDrillHole(Extrusion ext, int numColumns, int rows, int multipier) {
+		
+		if (multipier == 2) {
+			return "output/" + ext.getId() + "/drill_hole/"  + ext.getId().substring(0,2) + "_2020_dh" + columnPattern(numColumns) + rows + fileExtension;
+		}
+		return "output/" + ext.getId() + "/drill_hole/" + ext.getId().substring(0,2) + "_dh" + columnPattern(numColumns) + rows + fileExtension;
+	}
+
+	public static String nameAccessHole(Extrusion ext, AccessHole accessHole, int numColumns, int rows) {
+		return "output/" + ext.getId() + "/access_hole/"  + ext.getId().substring(0,2) + "X_ah" + columnPattern(numColumns) + rows +  fileExtension;
 	}
 
 	private static String columnPattern(int pattern) {
@@ -25,18 +44,6 @@ public class FileNameService {
 			throw new RuntimeException("Unsupported number of columns = " + pattern);
 		}
 		
-	}
-
-	public static String nameDrillHole(Extrusion ext, int numColumns, int rows, int multipier) {
-		
-		if (multipier == 2) {
-			return "output/" + ext.getId() + "/"  + ext.getId().substring(0,2) + "_2020_dh" + columnPattern(numColumns) + rows + fileExtension;
-		}
-		return "output/" + ext.getId() + "/"  + ext.getId().substring(0,2) + "_dh" + columnPattern(numColumns) + rows + fileExtension;
-	}
-
-	public static String nameAccessHole(Extrusion ext, AccessHole accessHole, int numColumns, int rows) {
-		return "output/" + ext.getId() + "/"  + ext.getId().substring(0,2) + "X_ah" + columnPattern(numColumns) + rows +  fileExtension;
 	}
 
 }
