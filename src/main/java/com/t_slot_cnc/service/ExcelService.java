201bf937956fb582002ffb8cc13db4922c10e7a0
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-06-22 14:56:06 -0600

    add missing service annotation

--- src/main/java/com/t_slot_cnc/service/ExcelService.java
+++ src/main/java/com/t_slot_cnc/service/ExcelService.java
@@ -11,7 +11,9 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ExcelService {
 
 	private static final String[] HEADERS = {"ID", "File", "Units", "Type",
