b3b423e24491ab6fc3be9f8900f9f46b14d6753c
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-06-24 01:02:28 -0600

    Launcher for JavaFX

---
+++ src/main/java/com/t_slot_cnc/TorpeyCNC.java
@@ -0,0 +1,13 @@
+package com.t_slot_cnc;
+
+/**
+ * This is the main program.  This is needed to avoid having to add the JavaFX module
+ * @author Alex Vazquez <vazqueza2000@gmail.com>
+ */
+public class TorpeyCNC {
+
+	public static void main(String[] args) {
+		TSlotCNCApplication.main(args);
+	}
+
+}
